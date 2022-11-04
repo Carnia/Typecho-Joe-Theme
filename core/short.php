e7fe2c11e613374b5a915eda2db49fdeda45f382
lq-hp <xlqdys@foxmail.com>
2022-11-04 14:32:52 +0800

    尝试引入编辑器

--- core/short.php
+++ core/short.php
@@ -4,7 +4,6 @@ function _parseContent($post, $login)
 {
     $content = $post->content;
     $content = _parseReply($content);
-    echo strpos($content, '{progress');
 
     if (strpos($content, '{lamp/}') !== false) {
         $content = strtr($content, array(
